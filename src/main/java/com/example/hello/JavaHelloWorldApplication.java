8eda45466e954ea043e876b52755a530127e6691
ximena <ximegames@gmail.com>
2025-12-04 11:37:37 -0600

    Hola mundo

---
+++ src/main/java/com/example/hello/JavaHelloWorldApplication.java
@@ -0,0 +1,23 @@
+package com.example.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class JavaHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaHelloWorldApplication.class, args);
+	}
+
+    @RestController
+    class HelloController {
+        @GetMapping("/")
+        public String hello (){
+            return "Hola mundo desde Cloud Run y Github!!";
+        }
+    }
+
+}
